b55f6b5896cf4b49a5f874e4aa781482117cd5a7
Ahmad Jamal <ahmadjamalcs@gmail.com>
2025-05-06 22:38:12 +0530

    vercel main funtion handler change

--- api/b.py
+++ api/b.py
@@ -235,5 +235,5 @@ def parse_assessment():
 def home():
     return "Hello from Flask on Vercel!"
 
-if __name__ == '__main__':
-    app.run()
+def handler(environ, start_response):
+    return app.wsgi_app(environ, start_response)
